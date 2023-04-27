891ce08a55fb31a070b94dd8e0bba0528eab3010
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2023-04-27 01:39:14 -0400

    am beginning to reconstruct code for react routing

--- corridoor-website/src/pages/_app.tsx
+++ corridoor-website/src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import '/Users/devk/Documents/GitHub/CorridoorWebsite/corridoor-website/src/pages/mainscreen/PopupTransition.css'; // Import the CSS file here
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
