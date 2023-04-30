298faeb49195422f5cf16d393d0d45e694763ae2
Dev Kunjadia <90876986+devk03@users.noreply.github.com>
2023-04-30 16:31:56 -0400

    Adjusted padding for main page to adhere better to mobile viewing

--- corridoor-website/src/components/homepage/homePage.tsx
+++ corridoor-website/src/components/homepage/homePage.tsx
@@ -15,7 +15,7 @@ const HomePage: React.FC = () => {
           px-4 // Add padding on the x-axis for small screens
         "
       >
-        <div className="pt-14">
+        <div className="lg:pt-14 sm:pt-4">
           <Logo />
         </div>
         <div className="font-bold	italic mt-8 mb-4 text-2xl md:text-2xl lg:text-4xl">
